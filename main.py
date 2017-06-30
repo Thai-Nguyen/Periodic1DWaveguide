1a5434151b5647f6bce3cff67ade72dabdb4f137
thain <thai.nguyen18@gmail.com>
2017-06-30 14:45:56 -0400

    refined convergence test.

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import matplotlib.pyplot as plt
 from assembly import assembly
 from solver import solver
 
-# Global parameters
-max_iterations = 100
 
 def preprocess():
     # Get element length in each region
@@ -15,9 +13,8 @@ def preprocess():
 
 def fem(le, beta_values, num_elements_in_region, total_num_elements, EpsilonR,
         MuR):
-
     A, B = assembly(num_elements_in_region, total_num_elements, le,
-                        beta_values, EpsilonR, MuR)
+                    beta_values, EpsilonR, MuR)
     eig_values, eig_vectors = solver(A, B)
     return eig_values, eig_vectors
 
@@ -51,8 +48,10 @@ def plot_electric_field(eig_value, eig_vector, beta, d):
     return None
 
 
-def plot_convergence(min_k0):
-    plt.plot(min_k0)
+def plot_convergence(total_num_elements, min_k0):
+    plt.plot(total_num_elements, min_k0)
+    plt.xlabel('Number of elements')
+    plt.ylabel('k_0')
     plt.show()
     return None
 
@@ -71,27 +70,30 @@ def sweep_parameter(beta_values, d, num_elements_in_region, total_num_elements, 
 
     # Sweep beta value and find corresponding k0
     print(np.size(beta_values), 'values to sweep')
-    print('Start:', beta_values[0], 'End:', beta_values[beta_values.size-1])
+    print('Start:', beta_values[0], 'End:', beta_values[beta_values.size - 1])
     for i in range(beta_values.size):
-        print('iter:', i+1, '/ beta:', beta_values[i])
+        print('iter:', i + 1, '/ beta:', beta_values[i])
         all_eig[:, i], _ = fem(le, beta_values[i], num_elements_in_region, total_num_elements, EpsilonR, MuR)
     plot_dispersion(all_eig, beta_values, d)
     return None
 
 
-def convergence_test(beta, EpsilonR, MuR):
-    # Allocate memory for smallest k0
-    min_k0 = np.zeros((max_iterations, 1))
+def convergence_test(beta, num_elements, EpsilonR, MuR):
+    # Allocate memory for smallest k0 and total number of elements
+    total_num_elements = np.zeros(num_elements.shape, dtype='int32')
+    min_k0 = np.zeros(num_elements.shape)
     le = preprocess()
 
     # Run convergence test
-    for i in range(1, max_iterations):
-        num_elements_in_region = np.array((10*i, 10*i))
-        total_num_elements = np.sum(num_elements_in_region)
-        eig_value, eig_vector = fem(le, beta, num_elements_in_region, total_num_elements, EpsilonR, MuR)
+    for i in range(num_elements.size):
+        num_elements_in_region = np.array((num_elements[i], num_elements[i]), dtype='int32')
+        total_num_elements[i] = np.sum(num_elements_in_region)
 
-        min_k0[i] = np.sqrt(eig_value[0])
-    plot_convergence(min_k0)
+        print('Number of elements', total_num_elements[i])
+        eig_value, eig_vector = fem(le, beta, num_elements_in_region, total_num_elements[i], EpsilonR, MuR)
+
+        min_k0[i] = np.sqrt(np.real(eig_value[0]))
+    plot_convergence(total_num_elements, min_k0)
     return None
 
 
@@ -113,4 +115,4 @@ if __name__ == '__main__':
 
     # run(beta_values[10], d, num_elements_in_region, total_num_elements, EpsilonR, MuR)
     # sweep_parameter(beta_values, d, num_elements_in_region, total_num_elements, EpsilonR, MuR)
-    convergence_test(beta_values[10], EpsilonR, MuR)
+    convergence_test(beta_values[10], np.arange(10, 500, 10, dtype='int32'), EpsilonR, MuR)
